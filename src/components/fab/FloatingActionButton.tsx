790a2fded5aef2d8bbc577252e0061b7e0849299
arcangeloizzo <arcangelo.izzo@gmail.com>
2025-11-15 00:14:16 +0100

    Update FloatingActionButton.tsx
    enlarge FAB

--- src/components/fab/FloatingActionButton.tsx
+++ src/components/fab/FloatingActionButton.tsx
@@ -20,7 +20,7 @@ export const FloatingActionButton = ({ onClick }: FloatingActionButtonProps) => 
       {/* Utilizziamo il componente Logo con variant="icon" e size="md" o "lg" */}
       <Logo 
         variant="icon" 
-        size="md" // Puoi provare "lg" se vuoi che sia più grande di w-8 h-8
+        size="lg" // Puoi provare "lg" se vuoi che sia più grande di w-8 h-8
         className="w-8 h-8 relative z-10" // Mantiene le dimensioni e classi attuali
       />
     </button>
